86f061a98346902b9cb89542e9bac69c24d54ab6
scoburto@gmail.com <scoburto@gmail.com>
2018-03-04 15:31:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ function divide(a,b) {
 }
 
 function inc(n) {
-  
+  return n++;
 }
 
 function dec(n) {
-  
+  return n--;
+}
+
+function makeInt(n) {
+  return parseInt(n);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
 }
