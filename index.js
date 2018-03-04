5a3efac468ee7dba2222e4e034dc34d4f00cf385
scoburto@gmail.com <scoburto@gmail.com>
2018-03-04 15:30:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(a,b) {
+  return b + a;
+}
+
+function subtract(a,b) {
+  return a - b;
+}
+
+function multiply() {
+  return b*a;
+}
+
+function divide(a,b) {
+  return a / b;
+}
+
+function inc(n) {
+  
+}
+
+function dec(n) {
+  
+}
